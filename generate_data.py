c012c0236a29db6e7315e94590598a6a6cee5c82
Lucas Martiniano <l231036@dac.unicamp.br>
2022-05-25 15:32:49 -0300

    support for data read

--- generate_data.py
+++ generate_data.py
@@ -4,12 +4,14 @@
 # Ax  Ay  Az      Gx  Gy  Gz
 
 ax,ay,az = 0, 0, 16348 # @ 2g - acccel
-gx,gy,gz = 0, 0, 0
+gx,gy,gz = 0, 100, 0
 
 with open('data', 'w') as f:
+  hit = 1
   for i in range(10000):
     f.write(f'{ax} {ay} {az} {gx} {gy} {gz}\n')
     print('ax ay az gx gy gz')
     print(f'{ax} {ay} {az} {gx} {gy} {gz}\n')
-    gy = 100
-    gx = -25
+    gx += 1
+    gy += 1
+    gz += 1
--- simu.py
+++ simu.py
@@ -4,28 +4,10 @@ app = Ursina()
 
 rocket = Entity(model='rocket.obj', collider='mesh')
 
-rocket.z += 500
 rocket.x -= 60
-
-def controls():
-  speed = 40
-  rocket.rotation_y += 0.4
-  rocket.rotation_x += 0.2
-  
-  camera.y += held_keys['w'] * time.dt * speed
-  camera.y -= held_keys['s'] * time.dt * speed
-  camera.x += held_keys['d'] * time.dt * speed
-  camera.x -= held_keys['a'] * time.dt * speed
-  camera.z += held_keys['e'] * time.dt * speed * 3
-  camera.z -= held_keys['q'] * time.dt * speed * 3
-
-  rocket.rotation_x -= held_keys['w'] * time.dt *speed
-  rocket.rotation_x += held_keys['s'] * time.dt *speed
-  rocket.rotation_y -= held_keys['d'] * time.dt *speed
-  rocket.rotation_y += held_keys['a'] * time.dt *speed
+rocket.z += 500
 
 def update():
-  #controls()
   ax,ay,az,gx,gy,gz = data.readline().strip().split()
   rocket.rotation_x = float(gx)
   rocket.rotation_y = float(gy)
